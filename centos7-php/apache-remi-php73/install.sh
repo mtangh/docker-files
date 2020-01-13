4f3b5439d26c9a96f80172fc5c40e2c44e3f2d7f
MT <mtan.web+github@gmail.com>
2020-01-14 06:31:46 +0900

    Update

---
+++ centos7-php/apache-remi-php73/install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -ux
+
+remirepo=remi-php73
+
+curl -sL -o remi.rpm "http://rpms.famillecollet.com/enterprise/remi-release-7.rpm" &&
+rpm -Uvh remi.rpm &&
+yum -v -y update &&
+yum -v -y install --enablerepo=${remirepo} php php-devel php-pear &&
+yum -v -y install --enablerepo=${remirepo} php-mbstring php-pdo php-gd &&
+yum -v -y install --enablerepo=${remirepo} php-xml php-json php-mcrypt php-pecl-zip &&
+yum -v -y clean all
+
+exit $?
